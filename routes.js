4bebd7cedb898d43f0275abe5271a539be41e85b
Ibeh Esther <estheribeh5@gmail.com>
2022-07-31 21:32:48 +0100

    refactor: complete and test all endpoints

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const {ObjectId} = require("mongodb");
+const {v4 : uuidv4} = require('uuid');
 const { db } = require("./models");
 
 const upload = multer({ dest: 'images'});
@@ -8,23 +9,17 @@ const app = express();
 const Event = db.collection('events');
 
 
-app.use(express.json())
-
-app.get('/api/v3/app/', (req, res) => {
-    res.send("Working");
-});
+app.use(express.json());
 
 // Gets an event by its unique id
 app.get('/api/v3/app/events', async(req, res) => {
-    var _id = req.query.id;
+    var uid = req.query.id;
     const {type, limit, page} = req.query;
-    if(_id){
+    if(uid){
         try{
-            _id= ObjectId(_id);
-            const event = await Event.find({_id}).toArray();
+            const event = await Event.find({uid}).toArray();
             if(event.length > 0){
                 res.send(event);
-                // console.log(event)
             }else{
                 throw new Error();
             }
@@ -34,20 +29,25 @@ app.get('/api/v3/app/events', async(req, res) => {
     }else{
         const startPage = (page - 1) * limit;
         const endPage = startPage + limit;
-        var events = await Event.find().sort({_id: -1}).toArray();
+        if (type == "latest"){
+            var events = await Event.find().sort({_id: -1}).toArray();
+        }else{
+            var events = await Event.find().sort({_id: 1}).toArray();
+        }
         events = events.slice(startPage, endPage);
-        res.send({events})
+        res.send({events});
     }
-    
 
 });
 
 // Creates a new event and returns the id of the newly created event
-app.post("/api/v3/app/events", upload.single('image'), (req, res) => {
-    const {attendees, ...event_params} = req.body;
-
+app.post("/api/v3/app/events", upload.single('files[image]'), (req, res) => {
+    var {attendees, rigor_rank} = req.body;
+    req.body.uid = uuidv4();
+    req.body.rigor_rank = parseInt(rigor_rank);
+    req.body.attendees =  eval('(' + attendees + ')');
     try{
-        Event.insertOne(event_params, (err, result) => {
+        Event.insertOne(req.body, (err, result) => {
             if(err){
                 throw new Error();
             }
@@ -58,23 +58,39 @@ app.post("/api/v3/app/events", upload.single('image'), (req, res) => {
     }catch(err){
         res.status(400).send(err);
     }
-})
+});
 
 // Updates an entire event with new data
-app.put('/api/v3/app/events/:id', async(req, res) => {
-    const _id = ObjectId(req.params.id);
-    const {name, tagline, schedule, description, 
-        moderator, category, sub_category, rigor_rank} 
-    = req.body;
+app.put('/api/v3/app/events/:id', upload.single('files[image]'), async(req, res) => {
+    const uid = req.params.id;
+    var {rigor_rank, attendees} = req.body;
+    req.body.rigor_rank = parseInt(rigor_rank);
+    req.body.attendees = eval('(' + attendees + ')');
+    console.log(req.body.attendees)
     try{
-        const updated_event = await Event.updateOne({_id}, {$set: {
-            name, tagline, schedule, description, 
-            moderator, category, sub_category, rigor_rank
+        const updated_event = await Event.updateOne({uid}, {$set: {
+            ...req.body
         }});
-       res.send(updated_event)
+       res.send(updated_event);
     }catch(err){
         res.status(400).send(err);
     }
-})
+});
+
+// Delete event by unique id
+app.delete('/api/v3/app/events/:id', async(req, res) => {
+    const uid = req.params.id;
+    console.log(uid)
+    try{
+        const event = await Event.deleteOne({uid});
+        if (event.deletedCount !== 0){
+            res.send('Event deleted successfully!');
+        }else{
+            throw new Error;
+        }
+    }catch(err){
+        res.status(404).send(err);
+    }
+});
 
 module.exports = app;
